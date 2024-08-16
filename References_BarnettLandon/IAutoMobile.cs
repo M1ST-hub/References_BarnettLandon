816feb0a443c62d2f9165375a01c6afdfbb2c545
M1ST-hub <143425337+M1ST-hub@users.noreply.github.com>
2024-08-16 08:21:28 -0500

    Initial commit

---
+++ References_BarnettLandon/IAutoMobile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace References_BarnettLandon
+{
+    internal interface IAutoMobile
+    {
+        //Interface that our automobile classes inherit from
+        public double Speed { get; }
+
+        public int Wheels { get; }
+
+        public string LicensePlate { get; }
+
+        public void Stringify();
+    }
+}
---
+++ References_BarnettLandon/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace References_BarnettLandon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // creates the first sedan
+            Sedan myFirstSedan = new Sedan(0);
+            IAutoMobile myAutomobile = myFirstSedan;
+            //creates a new sedan
+            Sedan myOtherSedan = new Sedan(0);
+            //prints speed to console and compares to other automobile
+            myFirstSedan.IncreaseSpeed();
+            Console.WriteLine(myFirstSedan.Speed);
+            Console.WriteLine(myAutomobile.Speed);
+            Console.WriteLine(myFirstSedan.Equals(myAutomobile));
+            //prints speed to console and compares to other automobile
+            myOtherSedan.IncreaseSpeed();
+            Console.WriteLine(myFirstSedan.Speed);
+            Console.WriteLine(myAutomobile.Speed);
+            Console.WriteLine(myFirstSedan.Equals(myOtherSedan));
+
+            Truck myTruck = new Truck(50, 500, "MyTruck");
+            //calls stringify method and describes automobiles
+            myFirstSedan.Stringify();
+            myAutomobile.Stringify();
+            myOtherSedan.Stringify();
+            myTruck.Stringify();
+        }
+    }
+}
